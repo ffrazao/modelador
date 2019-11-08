04b03c9c939d63081e948306517a5280f600a384
Fernando Frazao <fernando.frazao@emater.df.gov.br>
2019-11-08 08:41:02 -0300

    Versão inicial
    Versão inicial do projeto
    
    Signed-off-by: Fernando Frazao <fernando.frazao@emater.df.gov.br>

---
+++ src/main/java/com/modelador/ModeladorApplication.java
@@ -0,0 +1,13 @@
+package com.modelador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeladorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeladorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modelador/ModeladorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeladorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
